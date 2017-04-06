3a3f6b3a4cc63c95872166128405a4a5d295dfc0
Roman Yasinovskyy <yasiro01@luther.edu>
2017-04-06 12:39:02 -0500

    Exercise 3

---
+++ ex3/exercise3.cpp
@@ -0,0 +1,10 @@
+#include "exercise3.h"
+
+using namespace std;
+
+int main() {
+    cout << "# Exercise 3" << endl;
+
+    return 0;
+}
+
---
+++ ex3/exercise3.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+//Declare your own functions here
