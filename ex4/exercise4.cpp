82f4df95288c1ddd61161f1cb3ed5185f65c6af5
Roman Yasinovskyy <yasiro01@luther.edu>
2017-04-11 12:17:11 -0500

    Started Exerxise 4

---
+++ ex4/exercise4.cpp
@@ -0,0 +1,12 @@
+#include "exercise4.h"
+
+using namespace std;
+
+int main() {
+    cout << "# Exercise 4" << endl;
+    srand(time(0));
+    cout << "\n# Structures" << endl;
+
+
+    return 0;
+}
---
+++ ex4/exercise4.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+#include <cmath>
+
+using namespace std;
