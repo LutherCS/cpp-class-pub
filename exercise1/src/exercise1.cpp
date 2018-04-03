7622495818ad3cad73c7ea9ea5b13c49b274fddc
AustinReese1998 <reesau01@luther.edu>
2018-04-03 16:52:15 -0500

    got some work done

--- exercise1/src/exercise1.cpp
+++ exercise1/src/exercise1.cpp
@@ -2,16 +2,21 @@
 
 //Print "Hello World" to the default system output
 void task1() {    
-    //TODO: Implement task1
+    cout << "Hello World" << endl;
 }
 //Ask the user to enter their first name and print "Hello USERNAME"
 void task2() {
-    //TODO: Implement task2
+    string firstName;
+    cout << "What is your name fella?" << endl;
+    getline(cin, firstName);
+    cout << "Hello " << firstName << endl;
 }
 //Ask a user to enter their full name and print the initials (ie. John Doe should yield JD)
 void task3() {
-    //TODO: Implement task3
-}
+    string firstName, lastName;
+    cout << "What is your full name fella?" << endl;
+    cin >> firstName >> lastName;
+    cout << "Hello " << firstName[0] << lastName[0] << endl;}
 //Ask a user to enter their age and print "Minor" (if age is less than 21) or "Major" (if age is 21+)
 void task4() {
     //TODO: Implement task4
@@ -30,7 +35,13 @@ void task7() {
 }
 //Calculate and print sum of all multiples of 253 between 1 and 2018 (inclusive).
 void task8() {
-    //TODO: Implement task8
+    int sum = 0;
+    for (int i = 0; i <= 2018; i++){
+        if (i % 253 == 0){
+            sum += i;
+        }
+    }
+    cout << sum << endl;
 }
 //Calculate and print sum and product of all numbers between 1 and 20 (inclusive). Hint: use a proper data type to hold a very large value of the product. 
 void task9() {
