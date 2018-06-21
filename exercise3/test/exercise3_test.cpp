1e6907d5bd273b57b331cf83f0d6b42b261426ca
Roman Yasinovskyy <yasiro01@luther.edu>
2018-06-21 17:52:54 -0500

    Add tests to Exercise 3

--- exercise3/test/exercise3_test.cpp
+++ exercise3/test/exercise3_test.cpp
@@ -4,6 +4,39 @@
 #include "catch.hpp"
 #include "../src/exercise3.cpp"
  
+TEST_CASE ( "Print grades", "[task1]") {
+    string expected = "\
+A    2\n\
+B    2\n\
+C    3\n\
+D    2\n\
+F    1\n\
+";
+    const int size = 10;
+    int grades[] = {76, 91, 65, 70, 55, 78, 82, 87, 95, 60};
+    char fakeoutArr[1024];
+    stringstream fakeout(fakeoutArr);
+
+    streambuf *coutbuf = cout.rdbuf();
+    cout.rdbuf(fakeout.rdbuf());
+
+    task1(grades, size);
+    CHECK( fakeout.str() == expected );
+
+    cout.rdbuf(coutbuf);
+}
+
+TEST_CASE ( "Update grades", "[task2]") {
+    const int size = 10;
+    int grades[] = {76, 91, 65, 70, 55, 78, 82, 87, 95, 60};
+    int grades_upd[] = {81, 96, 70, 75, 60, 83, 87, 92, 100, 65};
+
+    task2(grades, size);
+    for (int i = 0; i < size; i++) {
+        CHECK( grades[i] == grades_upd[i] );
+    }
+}
+
 TEST_CASE( "Sum of all items", "[task3]" ) {
     int array0[] = {};
     int array1[] = {1, 3, 5, 7, 9};
@@ -17,27 +50,43 @@ TEST_CASE( "Sum of all items", "[task3]" ) {
 TEST_CASE( "Find an item in array", "[task4]" ) {
     int array1[] = {1, 3, 5, 7, 9};
     int array2[] = {2, 4, 6, 8, 10};
-    CHECK( task4(array1, 5, 7) );
+    CHECK( task4(array1, 5, 3) );
+    CHECK( task4(array2, 5, 4) );
+    CHECK_FALSE( task4(array1, 5, 6) );
     CHECK_FALSE( task4(array2, 5, 7) );
 }
 
 TEST_CASE( "Are the letters ordered", "[task5]" ) {
     CHECK( task5("buy") );
+    CHECK( task5("ahoy") );
+    CHECK_FALSE( task5("aloha") );
     CHECK_FALSE( task5("bye") );
 }
 
 TEST_CASE( "Find an item in vector", "[task6]" ) {
     vector<int> list1 = {1, 3, 5, 7, 9};
     vector<int> list2 = {2, 4, 6, 8, 10};
-    CHECK( task6(list1, 7) );
+    CHECK( task6(list1, 3) );
+    CHECK( task6(list2, 4) );
+    CHECK_FALSE( task6(list1, 6) );
     CHECK_FALSE( task6(list2, 7) );
+}
+
+TEST_CASE ( "Generate grades", "[task7]") {
+    const int size = 10;
+    vector<int> expected = {83, 86, 77, 65, 93, 85, 86, 92, 99, 71};
+    vector<int> result = task7(size);
+
+    CHECK( result == expected );
 }
 
 TEST_CASE( "Find items below threshold", "[task8]" ) {
     vector<int> list1 = {1, 3, 5, 7, 9};
     vector<int> list2 = {2, 4, 6, 8, 10};
+    CHECK( task8(list1, 0) == 0 );
     CHECK( task8(list1, 7) == 3 );
     CHECK( task8(list2, 1) == 0 );
+    CHECK( task8(list2, 10) == 4 );
 }
 
 TEST_CASE( "Reverse a vector", "[task9]" ) {
