b6fc2283b246caeda7f814258629a358df6aedcd
Roman Yasinovskyy <yasinovskyy@gmail.com>
2022-02-08 14:25:57 -0600

    Update exercise 1

--- exercise1/src/exercise1.hpp
+++ exercise1/src/exercise1.hpp
@@ -1,7 +1,7 @@
 /*
  * Assignment: Exercise 1
  * Author: Roman Yasinovskyy
- * Date: 2018-04-03
+ * Date: 2022-02-08
  */
 #ifndef EXERCISE1_H
 #define EXERCISE1_H
@@ -15,25 +15,25 @@
 
 using namespace std;
 
-//Print "Hello World" to the default system output
+// Print "Hello World" to the default system output
 void task1();
-//Ask the user to enter their first name and print "Hello USERNAME"
+// Ask the user to enter their first name and print "Hello name"
 void task2();
-//Ask a user to enter their full name and print the initials (ie. John Doe should yield JD)
+// Ask a user to enter their full name and print the initials (ie. John Doe should yield JD)
 void task3();
-//Ask a user to enter their age and print "Minor" (if age is less than 21) or "Major" (if age is 21+)
+// Ask a user to enter their age and print "Minor" (if age is less than 21) or "Major" (if age is 21+)
 void task4();
-//Generate a random number, print it, and print whether it's odd or even
+// Generate a random number, print it, and print whether it's odd or even
 void task5();
-//Print all numbers between 1 and 20 (inclusive).
+// Print all numbers between 1 and 20 (inclusive).
 void task6();
-//Calculate and print sum of all numbers between 1 and 100 (inclusive).
+// Calculate and print sum of all numbers between 1 and 100 (inclusive).
 void task7();
-//Calculate and print sum of all multiples of 253 between 1 and 2018 (inclusive).
+// Calculate and print sum of all multiples of 253 between 1 and 2022 (inclusive).
 void task8();
-//Calculate and print sum and product of all numbers between 1 and 20 (inclusive). Hint: use a proper data type to hold a very large value of the product. 
+// Calculate and print sum and product of all numbers between 1 and 20 (inclusive). Hint: use a proper data type to hold a very large value of the product. 
 void task9();
-//Print all numbers between 1 and 100 (inclusive) replacing all multiples of 3 with "fuzz", multiples of 5 with "buzz", multiples of 3 and 5 with "zzz".
+// Print all numbers between 1 and 100 (inclusive) replacing all multiples of 3 with "fuzz", multiples of 5 with "buzz", multiples of 3 and 5 with "zzz".
 void task10();
 
 #endif
--- exercise1/test/exercise1_test.cpp
+++ exercise1/test/exercise1_test.cpp
@@ -1,152 +1,154 @@
-/* 
- * This is an actual test file
+/*
+ * Exercise 1 test
  */
-#include "catch.hpp"
+#define CATCH_CONFIG_MAIN
+#include "../../catch2/catch.hpp"
 #include "../src/exercise1.cpp"
- 
-TEST_CASE( "Task 1", "[task1]" ) {
-    string expected = "Hello World\n";
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+TEST_CASE("Task 1", "[task1]") {
+  string expected = "Hello World\n";
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    task1();
-    CHECK( fakeout.str() == expected );
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    cout.rdbuf(coutbuf);
+  task1();
+  CHECK(fakeout.str() == expected);
+
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 2", "[task2]" ) {
-    string expected = "Enter your first name\nHello Roman\n";
-    stringstream fakein("Roman");
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
+TEST_CASE("Task 2", "[task2]") {
+  string expected = "Enter your first name\nHello Roman\n";
+  stringstream fakein("Roman");
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *cinbuf = cin.rdbuf();
-    cin.rdbuf(fakein.rdbuf());
+  streambuf *cinbuf = cin.rdbuf();
+  cin.rdbuf(fakein.rdbuf());
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    task2();
-    CHECK( fakeout.str() == expected );
+  task2();
+  CHECK(fakeout.str() == expected);
 
-    cin.rdbuf(cinbuf);
-    cout.rdbuf(coutbuf);
+  cin.rdbuf(cinbuf);
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 3", "[task3]" ) {
-    string expected = "Enter your full name\nYour initials are JD\n";
-    stringstream fakein("John Doe");
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
+TEST_CASE("Task 3", "[task3]") {
+  string expected = "Enter your full name\nYour initials are JD\n";
+  stringstream fakein("John Doe");
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *cinbuf = cin.rdbuf();
-    cin.rdbuf(fakein.rdbuf());
+  streambuf *cinbuf = cin.rdbuf();
+  cin.rdbuf(fakein.rdbuf());
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    task3();
-    CHECK( fakeout.str() == expected );
+  task3();
+  CHECK(fakeout.str() == expected);
 
-    cin.rdbuf(cinbuf);
-    cout.rdbuf(coutbuf);
+  cin.rdbuf(cinbuf);
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 4", "[task4]" ) {
-    string expected = "Enter your age\nMajor\n";
-    stringstream fakein("22");
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
-    
-    streambuf *cinbuf = cin.rdbuf();
-    cin.rdbuf(fakein.rdbuf());
+TEST_CASE("Task 4", "[task4]") {
+  string expected = "Enter your age\nMajor\n";
+  stringstream fakein("22");
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *cinbuf = cin.rdbuf();
+  cin.rdbuf(fakein.rdbuf());
 
-    task4();
-    CHECK( fakeout.str() == expected );
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    cin.rdbuf(cinbuf);
-    cout.rdbuf(coutbuf);
+  task4();
+  CHECK(fakeout.str() == expected);
+
+  cin.rdbuf(cinbuf);
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 5", "[task5]" ) {
-    string expected = "1804289383 is odd\n";
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
+TEST_CASE("Task 5", "[task5]") {
+  string expected = "1804289383 is odd\n";
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    task5();
-    CHECK( fakeout.str() == expected );
+  task5();
+  CHECK(fakeout.str() == expected);
 
-    cout.rdbuf(coutbuf);
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 6", "[task6]" ) {
-    string expected = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 \n";
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
+TEST_CASE("Task 6", "[task6]") {
+  string expected = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 \n";
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    task6();
-    CHECK( fakeout.str() == expected );
+  task6();
+  CHECK(fakeout.str() == expected);
 
-    cout.rdbuf(coutbuf);
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 7", "[task7]" ) {
-    string expected = "Sum of all numbers between 1 and 100 is 5050\n";
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
+TEST_CASE("Task 7", "[task7]") {
+  string expected = "Sum of all numbers between 1 and 100 is 5050\n";
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    task7();
-    CHECK( fakeout.str() == expected );
+  task7();
+  CHECK(fakeout.str() == expected);
 
-    cout.rdbuf(coutbuf);
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 8", "[task8]" ) {
-    string expected = "Sum of all multiples of 253 between 1 and 2018 is 7084\n";
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
+TEST_CASE("Task 8", "[task8]") {
+  string expected = "Sum of all multiples of 253 between 1 and 2018 is 7084\n";
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    task8();
-    CHECK( fakeout.str() == expected );
+  task8();
+  CHECK(fakeout.str() == expected);
 
-    cout.rdbuf(coutbuf);
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 9", "[task9]" ) {
-    string expected = "Product of all numbers between 1 and 20 is 2432902008176640000\n";
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
+TEST_CASE("Task 9", "[task9]") {
+  string expected =
+      "Product of all numbers between 1 and 20 is 2432902008176640000\n";
+  char fakeoutArr[1024];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    task9();
-    CHECK( fakeout.str() == expected );
+  task9();
+  CHECK(fakeout.str() == expected);
 
-    cout.rdbuf(coutbuf);
+  cout.rdbuf(coutbuf);
 }
 
-TEST_CASE( "Task 10", "[task10]" ) {
-    string expected = "\
+TEST_CASE("Task 10", "[task10]") {
+  string expected = "\
     1    2 fuzz    4 buzz fuzz    7    8 fuzz buzz\n\
    11 fuzz   13   14  zzz   16   17 fuzz   19 buzz\n\
  fuzz   22   23 fuzz buzz   26 fuzz   28   29  zzz\n\
@@ -158,14 +160,14 @@ TEST_CASE( "Task 10", "[task10]" ) {
  fuzz   82   83 fuzz buzz   86 fuzz   88   89  zzz\n\
    91   92 fuzz   94 buzz fuzz   97   98 fuzz buzz\n\
 ";
-    char fakeoutArr[1024];
-    stringstream fakeout(fakeoutArr);
+  char fakeoutArr[4096];
+  stringstream fakeout(fakeoutArr);
 
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(fakeout.rdbuf());
+  streambuf *coutbuf = cout.rdbuf();
+  cout.rdbuf(fakeout.rdbuf());
 
-    task10();
-    CHECK( fakeout.str() == expected );
+  task10();
+  CHECK(fakeout.str() == expected);
 
-    cout.rdbuf(coutbuf);
+  cout.rdbuf(coutbuf);
 }
